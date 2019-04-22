db627a1ab4ec483f8f851acf4731d289b335e002
Sandy Ludosky <s.ludosky@oodrive.com>
2019-04-22 14:15:24 +0200

    added DataSource > SearchResultDataSource

---
+++ iTunesSearchAPI/DataSource/SearchResultDataSource.swift
@@ -0,0 +1,43 @@
+//
+//  DataSource.swift
+//  iTunesSearchAPI
+//
+//  Created by Sandy Ludosky on 21/04/2019.
+//  Copyright © 2019 Sandy Ludosky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchResultDataSource: NSObject {
+    
+    private var results: [Result]
+  
+    init(items: [Result]) {
+        self.results = items
+        super.init()
+        
+    }
+    // MARK: - Helper
+    func update(with data: [Result]) {
+       results = data
+    }
+}
+
+ // MARK: - UICollectionViewDataSource
+extension SearchResultDataSource: UICollectionViewDataSource {
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.identifier, for: indexPath) as? SearchCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let result = results[indexPath.row]
+        let viewModel = SearchCollectionViewModel(with: result)
+        cell.configure(with: viewModel)
+        return cell
+    }
+}
