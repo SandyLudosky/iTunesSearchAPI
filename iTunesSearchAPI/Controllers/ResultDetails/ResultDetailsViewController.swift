5830d436f4f4557a35f33d972467edd1a104daca
Sandy Ludosky <s.ludosky@oodrive.com>
2019-04-23 20:20:39 +0200

    Ui update > Result DetailsVC

--- iTunesSearchAPI/Controllers/ResultDetails/ResultDetailsViewController.swift
+++ iTunesSearchAPI/Controllers/ResultDetails/ResultDetailsViewController.swift
@@ -67,6 +67,5 @@ extension ResultDetailsViewController: WKNavigationDelegate, WKUIDelegate {
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         loader.stopAnimating()
         print("did fail")
-        AlertDialogView.build(with: "error loading preview", vc: self)
     }
 }
